1f7b973bd9a02e1c142ae3f56d88268538d07c5c
DaniloIS <dailosantos-pg@hotmail.com>
2022-03-16 17:29:00 -0300

    Dependências do Template AdminLTE

---
+++ frontend/src/common/template/dependencies.js
@@ -0,0 +1,13 @@
+import 'module/admin-lte/plugins/jQueryUI/jquery-ui.min'
+import 'module/admin-lte/plugins/fastclick/fastclick'
+import 'module/admin-lte/plugins/slimScroll/jquery.slimscroll.min'
+import 'module/admin-lte/dist/js/app.min'
+
+import 'module/font-awesome/css/font-awesome.min.css'
+import 'module/ionicons/dist/css/ionicons.min.css'
+import 'module/admin-lte/bootstap/css/bootstap.min.css'
+import 'module/admin-lte/dist/css/adminLTE.min.css'
+import 'module/admin-lte/dist/css/skins/_all-skins.min.css'
+import 'module/admin-lte/plugins/iCheck/flat/blue.css'
+
+import './custom.css'
