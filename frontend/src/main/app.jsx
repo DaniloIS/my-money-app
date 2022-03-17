3fa843d40cc50ee7a4d4f8473fde5fd9b421fe10
DaniloIS <danilosantos-pg@hotmail.com>
2022-03-17 08:36:24 -0300

    Componente Footer

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Header from '../common/template/header';
 import SideBar from '../common/template/sideBar';
+import Footer from '../common/template/footer';
 
 import '../common/template/dependencies';
 
@@ -9,5 +10,9 @@ export default props => (
     <div className='wrapper'>
         <Header />
         <SideBar />
+        <div className="content-wrapper">
+            <h1>Conteudo</h1>
+        </div>
+        <Footer />
     </div>
 )
