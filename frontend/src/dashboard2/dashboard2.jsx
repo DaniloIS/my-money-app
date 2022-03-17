341c85097d1be451348017527834442a1094275c
DaniloIS <dailosantos-pg@hotmail.com>
2022-03-17 15:24:58 -0300

    Dashboard sem Redux

--- frontend/src/dashboard2/dashboard2.jsx
+++ frontend/src/dashboard2/dashboard2.jsx
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import ContentHeader from '../common/template/contentHeader';
 import Content from '../common/template/content';
 import ValueBox from '../common/widget/valueBox';
 import Row from '../common/layout/row';
 
+const BASE_URL = 'http://localhost:3003/api';
+
 export default class Dashboard2 extends Component {
+    
+    constructor(props) {
+        super(props)
+        this.state = {credit: 0, debt: 0}
+    }
 
     componentWillMount() {
         axios.get(`${BASE_URL}/billingCycles/summary`)
+            .then(res => this.setState(res.data))
     }
 
     render() {
-        const { credit, debt } = this.props.summary
+        const { credit, debt } = this.state
         return (
             <div>
                 <ContentHeader title='Dashboard' small='Versão 1.0' />
