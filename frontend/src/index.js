06ce8ab4cc59f651f7cfa6cb2ca80686fc00d111
DaniloIS <danilosantos-pg@hotmail.com>
2022-03-21 00:16:24 -0300

    Atualizando as Bibliotecas

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,19 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux';
+import registerServiceWorker from './registerServiceWorker'
 
 import promise from 'redux-promise';
 import multi from 'redux-multi';
 import thunk from 'redux-thunk';
 
-import Routes from './main/routes';
+import AuthOrApp from './main/authOrApp';
 import reducers from './main/reducers';
 
 const store = applyMiddleware(promise, multi, thunk)(createStore)(reducers);
 
 ReactDOM.render(
     <Provider store={store}>
-        <Routes />
+        <AuthOrApp />
     </Provider>, 
     document.getElementById('app')
 );
+registerServiceWorker()
